0ac6cf6b05af2bcef7a55f6d2e585816bdd36842
CyberPhoenixArchitecture <161476848+CyberPhoenixArchitecture@users.noreply.github.com>
2024-07-27 10:32:32 +0700

    Create exception.c

---
+++ src/exception.c
@@ -0,0 +1,245 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <dbghelp.h>
+#include "addr.c"
+
+static inline const char *GetExceptionMessage(DWORD ExceptionCode)
+{
+    switch (ExceptionCode)
+    {
+        case 0xC0000005: // EXCEPTION_ACCESS_VIOLATION
+            return "access violation";
+        case 0xC0000006: // EXCEPTION_IN_PAGE_ERROR
+            return "in page error";
+        /*case 0xC0000008: // EXCEPTION_INVALID_HANDLE
+            return "invalid handle";*/
+        case 0xC000001D: // EXCEPTION_ILLEGAL_INSTRUCTION
+            return "illegal instruction";
+        case 0xC0000025: // EXCEPTION_NONCONTINUABLE_EXCEPTION
+            return "cannot continue";
+        case 0xC0000026: // EXCEPTION_INVALID_DISPOSITION
+            return "invalid disposition";
+        case 0xC000008C: // EXCEPTION_ARRAY_BOUNDS_EXCEEDED
+            return "array bounds exceeded";
+        case 0xC000008D: // EXCEPTION_FLT_DENORMAL_OPERAND
+            return "floating-point denormal operand";
+        case 0xC000008E: // EXCEPTION_FLT_DIVIDE_BY_ZERO
+            return "floating-point division by zero";
+        case 0xC000008F: // EXCEPTION_FLT_INEXACT_RESULT
+            return "floating-point inexact result";
+        case 0xC0000090: // EXCEPTION_FLT_INVALID_OPERATION
+            return "floating-point invalid operation";
+        case 0xC0000091: // EXCEPTION_FLT_OVERFLOW
+            return "floating-point overflow";
+        case 0xC0000092: // EXCEPTION_FLT_STACK_CHECK
+            return "floating-point stack check";
+        case 0xC0000093: // EXCEPTION_FLT_UNDERFLOW
+            return "floating-point underflow";
+        case 0xC0000094: // EXCEPTION_INT_DIVIDE_BY_ZERO
+            return "integer division by zero";
+        case 0xC0000095: // EXCEPTION_INT_OVERFLOW
+            return "integer overflow";
+        case 0xC0000096: // EXCEPTION_PRIV_INSTRUCTION
+            return "privileged instruction";
+        case 0xC00000FD: // EXCEPTION_STACK_OVERFLOW
+            return "stack overflow";
+        /*case 0xC0000194: // EXCEPTION_POSSIBLE_DEADLOCK
+            return "possible deadlock condition";
+        case 0xC0000409: // STATUS_STACK_BUFFER_OVERRUN
+            //https://devblogs.microsoft.com/oldnewthing/20190108-00/?p=100655
+            return "fast fail";
+        case 0xC000041D: // STATUS_FATAL_USER_CALLBACK_EXCEPTION
+            return "fatal user callback exception";
+        case 0xC0000420: // STATUS_ASSERTION_FAILURE
+            return "assertion failure";
+        case 0xE0434f4D: // STATUS_CLR_EXCEPTION
+            return "CLR exception";
+        case 0xE06D7363: // STATUS_CPP_EH_EXCEPTION
+            return "C++ exception handling exception";
+        case 0x80000001: // EXCEPTION_GUARD_PAGE
+            return "guard page exception";*/
+        case 0x80000002: // EXCEPTION_DATATYPE_MISALIGNMENT
+            return "alignment fault";
+        /*case 0x80000003: // EXCEPTION_BREAKPOINT
+            return "breakpoint";*/
+        case 0x80000004: // EXCEPTION_SINGLE_STEP
+            return "single step";
+        /*case 0x4000001F: // STATUS_WX86_BREAKPOINT
+            return "breakpoint";*/
+        /*case 0x40000015: // STATUS_FATAL_APP_EXIT
+            return "fast application exit";
+        case 0x40010003: // DBG_TERMINATE_THREAD
+            return "terminate thread";
+        case 0x40010004: // DBG_TERMINATE_PROCESS
+            return "terminate process";*/
+        case 0x40010005: // DBG_CONTROL_C
+            return "CONTROL+C";
+        /*case 0x40010008: // DBG_CONTROL_BREAK
+            return "CONTROL+BREAK";
+        case 0x406D1388:
+            return "thread name exception";
+        case 1717: //RPC_S_UNKNOWN_IF
+            return "unknown interface";
+        case 1722: //RPC_S_SERVER_UNAVAILABLE
+            return "server unavailable";*/
+        default:
+            return "unknown exception";
+    }
+}
+
+static inline char *FormatDebugException(DEBUG_EVENT *DebugEvent, char *buffer, char *p, BOOL bWow64)
+{
+    int temp;
+    const char *ptr = GetExceptionMessage(DebugEvent->u.Exception.ExceptionRecord.ExceptionCode);
+    temp = strlen(ptr);
+    memcpy(buffer, ptr, temp);
+    buffer += temp;
+    if (DebugEvent->u.Exception.ExceptionRecord.ExceptionCode == 0xC0000005 ||
+        DebugEvent->u.Exception.ExceptionRecord.ExceptionCode == 0xC0000006)
+    {
+        *buffer = ' ';
+        ++buffer;
+        switch (DebugEvent->u.Exception.ExceptionRecord.ExceptionInformation[0])
+        {
+            case 0:
+                memcpy(buffer, "reading from", 12);
+                buffer += 12;
+                break;
+            case 1:
+                memcpy(buffer, "writing to", 10);
+                buffer += 10;
+                break;
+            case 8:
+                memcpy(buffer, "DEP violation at", 16);
+                buffer += 16;
+                break;
+        }
+        memcpy(buffer, " location 0x", 12);
+        buffer += 12;
+        if (bWow64) _ultoaddr((ULONGLONG) DebugEvent->u.Exception.ExceptionRecord.ExceptionInformation[1], buffer, p);
+        else ulltoaddr((ULONGLONG) DebugEvent->u.Exception.ExceptionRecord.ExceptionInformation[1], buffer, p);
+        buffer += 16;
+    }
+    return buffer;
+}
+
+#define FormatVerboseDebugException(p, ExceptionCode) \
+({ \
+    switch (ExceptionCode) \
+    { \
+        case 0xC0000005: \
+            memcpy(p, "The thread tried to read from or write to a virtual address for which it does not have the appropriate access", 109); \
+            p += 109; \
+            break; \
+        case 0xC0000006: \
+            memcpy(p, "The thread tried to access a page that was not present, and the system was unable to load the page", 98); \
+            p += 98; \
+            break; \
+        case 0xC000001D: \
+            memcpy(p, "The thread tried to execute an invalid instruction", 50); \
+            p += 50; \
+            break; \
+        case 0xC0000025: \
+            memcpy(p, "The thread tried to continue execution after a noncontinuable exception occurred", 80); \
+            p += 80; \
+            break; \
+        case 0xC0000026: \
+            memcpy(p, "An exception handler returned an invalid disposition to the exception dispatcher", 80); \
+            p += 80; \
+            break; \
+        case 0xC000008C: \
+            memcpy(p, "The thread tried to access an array element that is out of bounds and the underlying hardware supports bounds checking", 118); \
+            p += 118; \
+            break; \
+        case 0xC000008D: \
+            memcpy(p, "One of the operands in a floating-point operation is denormal. A denormal value is one that is too small to represent as a standard floating-point value", 152); \
+            p += 152; \
+            break; \
+        case 0xC000008E: \
+            memcpy(p, "The thread tried to divide a floating-point value by a floating-point divisor of zero", 85); \
+            p += 85; \
+            break; \
+        case 0xC000008F: \
+            memcpy(p, "The result of a floating-point operation cannot be represented exactly as a decimal fraction", 92); \
+            p += 92; \
+            break; \
+        case 0xC0000090: \
+            memcpy(p, "This exception represents any floating-point exception not included", 67); \
+            p += 67; \
+            break; \
+        case 0xC0000091: \
+            memcpy(p, "The exponent of a floating-point operation is greater than the magnitude allowed by the corresponding type", 106); \
+            p += 106; \
+            break; \
+        case 0xC0000092: \
+            memcpy(p, "The stack overflowed or underflowed as the result of a floating-point operation", 79); \
+            p += 79; \
+            break; \
+        case 0xC0000093: \
+            memcpy(p, "The exponent of a floating-point operation is less than the magnitude allowed by the corresponding type", 103); \
+            p += 103; \
+            break; \
+        case 0xC0000094: \
+            memcpy(p, "The thread tried to divide an integer value by an integer divisor of zero", 73); \
+            p += 73; \
+            break; \
+        case 0xC0000095: \
+            memcpy(p, "The result of an integer operation caused a carry out of the most significant bit of the result", 95); \
+            p += 95; \
+            break; \
+        case 0xC0000096: \
+            memcpy(p, "The thread tried to execute an instruction whose operation is not allowed in the current machine mode", 101); \
+            p += 101; \
+            break; \
+        case 0xC00000FD: \
+            memcpy(p, "The thread used up its stack", 28); \
+            p += 28; \
+            break; \
+        case 0x80000002: \
+            memcpy(p, "The thread tried to read or write data that is misaligned on hardware that does not provide alignment", 101); \
+            p += 101; \
+            break; \
+        case 0x80000004: \
+            memcpy(p, "A trace trap or other single-instruction mechanism signaled that one instruction has been executed", 98); \
+            p += 98; \
+            break; \
+    } \
+    p; \
+})
+
+static inline char *FormatSourceCode(char *fname, int line, char *p)
+{
+    int temp;
+    char buffer[4096];
+    FILE *fp;
+    temp = 0;
+    fp = fopen(fname, "r");
+    while (!fgets(buffer, sizeof(buffer), fp))
+    {
+        ++temp;
+        if (temp == line)
+        {
+            p += sprintf(p, "%8u | %s", line, buffer);
+            break;
+        }
+    }
+    return p;
+}
+
+static inline char *FormatFileLine(IMAGEHLP_LINE64 *lpLine, char *p)
+{
+    int temp;
+    temp = strlen(lpLine->FileName);
+    memcpy(p, lpLine->FileName, temp);
+    p += temp;
+    *p = ':';
+    ++p;
+    _ultoa(lpLine->LineNumber, p, 10);
+    p += strlen(p);
+    *p = '\n';
+    ++p;
+    return FormatSourceCode(lpLine->FileName, lpLine->LineNumber, p);
+}
