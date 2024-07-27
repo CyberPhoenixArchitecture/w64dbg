7ce8e65b9c67c8d9e19300d04af95b1953037c20
CyberPhoenixArchitecture <161476848+CyberPhoenixArchitecture@users.noreply.github.com>
2024-07-27 10:31:51 +0700

    Create 2.cpp

---
+++ sample/2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+static inline void Exception(int i, double j, const char * str)
+{
+    sscanf("12345", "%d", (int *) 1);
+}
+
+struct Example
+{
+    static void RootException(int i, float j)
+    {
+        Exception(i * 2, j, "Hello");
+    }
+
+    void CauseException(void)
+    {
+        RootException(4, 5.6f);
+    }
+};
+
+int main(void)
+{
+    Example example;
+    example.CauseException();
+}
