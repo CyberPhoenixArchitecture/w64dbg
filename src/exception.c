f8f78ed755bf7be9281265d889b25e6bddf68db1
CyberPhoenixArchitecture <161476848+CyberPhoenixArchitecture@users.noreply.github.com>
2024-08-22 11:13:44 +0700

    Update exception.c

--- src/exception.c
+++ src/exception.c
@@ -14,8 +14,8 @@ static inline const char *GetExceptionMessage(DWORD ExceptionCode)
             return "access violation";
         case 0xC0000006: // EXCEPTION_IN_PAGE_ERROR
             return "in page error";
-        /*case 0xC0000008: // EXCEPTION_INVALID_HANDLE
-            return "invalid handle";*/
+        case 0xC0000008: // EXCEPTION_INVALID_HANDLE
+            return "invalid handle";
         case 0xC000001D: // EXCEPTION_ILLEGAL_INSTRUCTION
             return "illegal instruction";
         case 0xC0000025: // EXCEPTION_NONCONTINUABLE_EXCEPTION
@@ -46,7 +46,7 @@ static inline const char *GetExceptionMessage(DWORD ExceptionCode)
             return "privileged instruction";
         case 0xC00000FD: // EXCEPTION_STACK_OVERFLOW
             return "stack overflow";
-        /*case 0xC0000194: // EXCEPTION_POSSIBLE_DEADLOCK
+        case 0xC0000194: // EXCEPTION_POSSIBLE_DEADLOCK
             return "possible deadlock condition";
         case 0xC0000409: // STATUS_STACK_BUFFER_OVERRUN
             //https://devblogs.microsoft.com/oldnewthing/20190108-00/?p=100655
@@ -60,31 +60,31 @@ static inline const char *GetExceptionMessage(DWORD ExceptionCode)
         case 0xE06D7363: // STATUS_CPP_EH_EXCEPTION
             return "C++ exception handling exception";
         case 0x80000001: // EXCEPTION_GUARD_PAGE
-            return "guard page exception";*/
+            return "guard page exception";
         case 0x80000002: // EXCEPTION_DATATYPE_MISALIGNMENT
             return "alignment fault";
-        /*case 0x80000003: // EXCEPTION_BREAKPOINT
-            return "breakpoint";*/
+        case 0x80000003: // EXCEPTION_BREAKPOINT
+            return "breakpoint";
         case 0x80000004: // EXCEPTION_SINGLE_STEP
             return "single step";
-        /*case 0x4000001F: // STATUS_WX86_BREAKPOINT
-            return "breakpoint";*/
-        /*case 0x40000015: // STATUS_FATAL_APP_EXIT
+        case 0x4000001F: // STATUS_WX86_BREAKPOINT
+            return "breakpoint";
+        case 0x40000015: // STATUS_FATAL_APP_EXIT
             return "fast application exit";
         case 0x40010003: // DBG_TERMINATE_THREAD
             return "terminate thread";
         case 0x40010004: // DBG_TERMINATE_PROCESS
-            return "terminate process";*/
+            return "terminate process";
         case 0x40010005: // DBG_CONTROL_C
             return "CONTROL+C";
-        /*case 0x40010008: // DBG_CONTROL_BREAK
+        case 0x40010008: // DBG_CONTROL_BREAK
             return "CONTROL+BREAK";
         case 0x406D1388:
             return "thread name exception";
         case 1717: //RPC_S_UNKNOWN_IF
             return "unknown interface";
         case 1722: //RPC_S_SERVER_UNAVAILABLE
-            return "server unavailable";*/
+            return "server unavailable";
         default:
             return "unknown exception";
     }
@@ -137,6 +137,10 @@ static inline char *FormatDebugException(DEBUG_EVENT *DebugEvent, char *buffer, 
         case 0xC0000006: \
             memcpy(p, "The thread tried to access a page that was not present, and the system was unable to load the page", 98); \
             p += 98; \
+            break; \
+        case 0xC0000008: \
+            memcpy(p, "The thread used a handle to a kernel object that was invalid", 60); \
+            p += 60; \
             break; \
         case 0xC000001D: \
             memcpy(p, "The thread tried to execute an invalid instruction", 50); \
@@ -198,13 +202,81 @@ static inline char *FormatDebugException(DEBUG_EVENT *DebugEvent, char *buffer, 
             memcpy(p, "The thread used up its stack", 28); \
             p += 28; \
             break; \
+        case 0xC0000194: \
+            memcpy(p, "The wait operation on the critical section times out", 52); \
+            p += 52; \
+            break; \
+        case 0xC0000409: \
+            memcpy(p, "The system detected an overrun of a stack-based buffer in this application", 74); \
+            p += 74; \
+            break; \
+        case 0xC000041D: \
+            memcpy(p, "An unhandled exception was encountered during a user callback", 61); \
+            p += 61; \
+            break; \
+        case 0xC0000420: \
+            memcpy(p, "An assertion failure has occurred", 33); \
+            p += 33; \
+            break; \
+        case 0xE0434f4D: \
+            memcpy(p, "A managed code exception was encountered within .NET Common Language Runtime", 76); \
+            p += 76; \
+            break; \
+        case 0xE06D7363: \
+            memcpy(p, "A C++ exception has been thrown and is being handled or caught", 62); \
+            p += 62; \
+            break; \
+        case 0x80000001: \
+            memcpy(p, "The thread accessed memory allocated with the PAGE_GUARD modifier", 65); \
+            p += 65; \
+            break; \
         case 0x80000002: \
             memcpy(p, "The thread tried to read or write data that is misaligned on hardware that does not provide alignment", 101); \
             p += 101; \
             break; \
+        case 0x80000003: \
+            memcpy(p, "A breakpoint was encountered", 28); \
+            p += 28; \
+            break; \
         case 0x80000004: \
             memcpy(p, "A trace trap or other single-instruction mechanism signaled that one instruction has been executed", 98); \
             p += 98; \
+            break; \
+        case 0x4000001F: \
+            memcpy(p, "A Win32 x86 breakpoint was encountered", 38); \
+            p += 38; \
+            break; \
+        case 0x40000015: \
+            memcpy(p, "The application caused an unhandled runtime exception during shutdown", 69); \
+            p += 69; \
+            break; \
+        case 0x40010003: \
+            memcpy(p, "The debugger terminated thread", 30); \
+            p += 30; \
+            break; \
+        case 0x40010004: \
+            memcpy(p, "The debugger terminated process", 31); \
+            p += 31; \
+            break; \
+        case 0x40010005: \
+            memcpy(p, "The debugger got control C", 26); \
+            p += 26; \
+            break; \
+        case 0x40010008: \
+            memcpy(p, "The debugger received control break", 35); \
+            p += 35; \
+            break; \
+        case 0x406D1388: \
+            memcpy(p, "The thread set its own name by raising exception", 48); \
+            p += 48; \
+            break; \
+        case 1717: \
+            memcpy(p, "The interface is unknown", 24); \
+            p += 24; \
+            break; \
+        case 1722: \
+            memcpy(p, "The RPC server is unavailable", 29); \
+            p += 29; \
             break; \
     } \
     p; \
@@ -217,7 +289,12 @@ static inline char *FormatSourceCode(char *fname, int line, char *p)
     FILE *fp;
     temp = 0;
     fp = fopen(fname, "r");
-    while (!fgets(buffer, sizeof(buffer), fp))
+    if (fp == NULL)
+    {
+        p += sprintf(p, "%s: No such file or directory.\n", fname);
+        return p;
+    }
+    while (fgets(buffer, sizeof(buffer), fp))
     {
         ++temp;
         if (temp == line)
@@ -226,6 +303,7 @@ static inline char *FormatSourceCode(char *fname, int line, char *p)
             break;
         }
     }
+    fclose(fp);
     return p;
 }
 
