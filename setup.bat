d6e17e466e76272872f7e21480e0ffcadf7987f6
CyberPhoenixArchitecture <161476848+CyberPhoenixArchitecture@users.noreply.github.com>
2024-08-27 14:04:03 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,7 @@
 @echo off
 set _NO_DEBUG_HEAP=1
 set GDB_SUPPRESS_SOURCES_WARNING=1
+del /f /q /ah %temp%\gdbinit
 where gdb.exe >nul 2>&1
 if %ERRORLEVEL% NEQ 0 echo [WARNING] gdb.exe not found, cannot use /G options.
 timeout -1
