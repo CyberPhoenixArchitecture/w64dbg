5ab00e79ebeed4295b545b4960fb7e982aeaf3a0
CyberPhoenixArchitecture <161476848+CyberPhoenixArchitecture@users.noreply.github.com>
2024-08-29 10:28:07 +0700

    Update optimize.h

--- src/optimize.h
+++ src/optimize.h
@@ -1,19 +1,14 @@
 #ifdef __OPTIMIZE__
 #undef memcpy
 #undef strcpy
-#if defined(__has_builtin)
+#ifdef __GNUC__
 #define memcpy(_Dst, _Src, _Size) \
 ({ \
     if (__builtin_constant_p(_Src) || \
         __builtin_constant_p(_Size)) \
-    { \
-        char *_Dst = \
-            (char *) __builtin_assume_aligned(_Dst, _Size << 3); \
-        const char *_Src = \
-            (const char *) __builtin_assume_aligned(_Src, _Size << 3); \
         for (unsigned int i = 0; i < _Size; ++i) \
-            _Dst[i] = _Src[i]; \
-    } \
+            ((char *) __builtin_assume_aligned(_Dst, _Size << 3))[i] = \
+                ((const char *) __builtin_assume_aligned(_Src, _Size << 3))[i]; \
     else __builtin_memcpy(_Dst, _Src, _Size); \
     _Dst; \
 })
@@ -21,30 +16,26 @@
 ({ \
     if (__builtin_constant_p(_Src)) \
         memcpy(_Dst, _Src, strlen(_Src)); \
-    else __builtin_strcpy(_Dst, _Src, _Size); \
+    else __builtin_strcpy(_Dst, _Src); \
     _Dst; \
 })
 #else
+#include <cstring>
+#include <type_traits>
+using namespace std;
 #define memcpy(_Dst, _Src, _Size) \
 ({ \
-    if (__is_constant_evaluated(_Src) || \
-        __is_constant_evaluated(_Size)) \
-    { \
-        char *_Dst = \
-            (char *) assume_aligned<_Size << 3>(_Dst); \
-        const char *_Src = \
-            (const char *) assume_aligned<_Size << 3>(_Src); \
+    if (__is_constant_evaluated()) \
         for (unsigned int i = 0; i < _Size; ++i) \
-            _Dst[i] = _Src[i]; \
-    } \
+            ((char *) _Dst)[i] = ((const char *) _Src)[i]; \
     else memcpy(_Dst, _Src, _Size); \
     _Dst; \
 })
 #define strcpy(_Dst, _Src) \
 ({ \
-    if (__is_constant_evaluated(_Src)) \
+    if (__is_constant_evaluated()) \
         memcpy(_Dst, _Src, strlen(_Src)); \
-    else strcpy(_Dst, _Src, _Size); \
+    else strcpy(_Dst, _Src); \
     _Dst; \
 })
 #endif
