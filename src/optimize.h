9cd041e8104158946ccccf72522904041d531db9
CyberPhoenixArchitecture <161476848+CyberPhoenixArchitecture@users.noreply.github.com>
2024-08-29 10:06:50 +0700

    Update optimize.h

--- src/optimize.h
+++ src/optimize.h
@@ -1,38 +1,53 @@
 #include <string.h>
 
-#if defined(__OPTIMIZE__) && defined(__has_attribute) && __has_builtin(__builtin_constant_p)
+#if defined(__OPTIMIZE__) && defined(__has_builtin)
 #undef memcpy
-#if __has_builtin(__builtin_memcpy)
+#undef strcpy
+#if __has_builtin(__builtin_constant_p)
 #define memcpy(_Dst, _Src, _Size) \
 ({ \
     if (__builtin_constant_p(_Src) || \
         __builtin_constant_p(_Size)) \
+    { \
+        char *_Dst = \
+            (char *) __builtin_assume_aligned(_Dst, _Size << 3); \
+        const char *_Src = \
+            (const char *) __builtin_assume_aligned(_Src, _Size << 3); \
         for (unsigned int i = 0; i < _Size; ++i) \
-            (_Dst)[i] = (_Src)[i]; \
+            _Dst[i] = _Src[i]; \
+    } \
     else __builtin_memcpy(_Dst, _Src, _Size); \
+    _Dst; \
+})
+#define strcpy(_Dst, _Src) \
+({ \
+    if (__builtin_constant_p(_Src)) \
+        memcpy(_Dst, _Src, strlen(_Src)); \
+    else __builtin_strcpy(_Dst, _Src, _Size); \
     _Dst; \
 })
 #else
 #define memcpy(_Dst, _Src, _Size) \
 ({ \
-    if (__builtin_constant_p(_Src) || \
-        __builtin_constant_p(_Size)) \
+    if (__is_constant_evaluated(_Src) || \
+        __is_constant_evaluated(_Size)) \
+    { \
+        char *_Dst = \
+            (char *) assume_aligned<_Size << 3>(_Dst); \
+        const char *_Src = \
+            (const char *) assume_aligned<_Size << 3>(_Src); \
         for (unsigned int i = 0; i < _Size; ++i) \
-            (_Dst)[i] = (_Src)[i]; \
+            _Dst[i] = _Src[i]; \
+    } \
     else memcpy(_Dst, _Src, _Size); \
     _Dst; \
 })
-#endif
-#if __has_builtin(__builtin_strlen)
-#define strlen(...) __builtin_strlen(__VA_ARGS__)
-#endif
-#if __has_builtin(__builtin_memchr)
-#define memchr(...) __builtin_memchr(__VA_ARGS__)
-#endif
-#if __has_builtin(__builtin_memcmp)
-#define memcmp(...) __builtin_memcmp(__VA_ARGS__)
-#endif
-#if __has_builtin(__builtin_strstr)
-#define strstr(...) __builtin_strstr(__VA_ARGS__)
+#define strcpy(_Dst, _Src) \
+({ \
+    if (__is_constant_evaluated(_Src)) \
+        memcpy(_Dst, _Src, strlen(_Src)); \
+    else strcpy(_Dst, _Src, _Size); \
+    _Dst; \
+})
 #endif
 #endif
