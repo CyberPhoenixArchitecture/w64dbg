469e111f487c1dda43d446d21739d5aa26559dee
CyberPhoenixArchitecture <161476848+CyberPhoenixArchitecture@users.noreply.github.com>
2024-09-14 21:00:04 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,4 +4,6 @@ set GDB_SUPPRESS_SOURCES_WARNING=1
 del /f /q /ah %temp%\gdbinit
 where gdb.exe >nul 2>&1
 if %ERRORLEVEL% NEQ 0 echo [WARNING] gdb.exe not found, cannot use /G options.
+where ucrtbase.dll >nul 2>&1
+if %ERRORLEVEL% NEQ 0 echo [ERROR] ucrtbase.dll missing. Install it here: https://www.dll-files.com/ucrtbase.dll.html
 timeout -1
